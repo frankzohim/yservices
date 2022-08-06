84d636200fe10460ff96267e8b74ea466d2701ca
level237 <Bramslevel129@gmail.com>
2022-08-06 17:16:54 +0200

    update

--- database/migrations/2022_08_06_150207_create_information_table.php
+++ database/migrations/2022_08_06_150207_create_information_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('information', function (Blueprint $table) {
+        Schema::create('informations', function (Blueprint $table) {
             $table->id();
             $table->string('type');
             $table->string('available');
@@ -34,6 +34,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('information');
+        Schema::dropIfExists('informations');
     }
 };
