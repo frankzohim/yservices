69b3211c7683f68a942feec3b4a11ecd9e3ce523
level237 <Bramslevel129@gmail.com>
2022-08-07 11:56:48 +0200

    Relation One to Many

--- database/migrations/2022_08_06_145306_create_franchises_table.php
+++ database/migrations/2022_08_06_145306_create_franchises_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,8 +14,13 @@ return new class extends Migration
      */
     public function up()
     {
+        Schema::disableForeignKeyConstraints();
         Schema::create('franchises', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(User::class)
+                ->constrained()
+                ->onUpdate('restrict')
+                ->onDelete('restrict');
             $table->string('personnal_contribution');
             $table->string('project_info');
             $table->timestamps();
