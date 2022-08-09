8566e6758220f3b8483ef3c1146a573a7ef0047a
Frank  Zohim <69459460+frankzohim@users.noreply.github.com>
2022-08-09 17:51:53 +0200

    Commit from monaco, login and registration added

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,8 +2,8 @@
 @section('title', __('Inscription'))
 
 @section('content')
-    <section class="vh-100" style="background-color: white;">
-        <div class="container ">
+    <section class="vh-100 " style="background-color: white;">
+        <div class="container " style="margin-top:25px; ">
           <div class="row d-flex justify-content-center align-items-center h-100">
             <div class="col-lg-12 col-xl-11">
                   <div class="row justify-content-center">
@@ -62,17 +62,21 @@
                           </label>
                         </div>
 
-                        <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                        <div class="d-flex justify-content-center ">
                           <button type="submit" class="btn btn-primary btn-lg" style="padding-left: 100px;padding-right:100px">Créer mon compte</button>
                         </div>
 
+                        <div class="d-flex justify-content-center" style="margin-top:10px;">
+                            <span style="margin-right: 20px; font-size:25px; font-weight:bold">Déjà Inscrit?</span>
+                         <a href="{{ route('login') }}" class="btn btn-success">Connectez-vous</a>
+                        </div>
                       </form>
 
                     </div>
                     <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
 
-                      <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
-                        class="img-fluid" alt="Sample image">
+                      <img src="{{ asset("_next/static/media/create_account.png") }}"
+                        class="img-fluid" alt="Sample image" width="70%">
 
                     </div>
                   </div>
