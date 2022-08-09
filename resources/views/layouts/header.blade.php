ee6962ff243dd11f69a6a0a18179a5ddb2c75fa7
Frank  Zohim <69459460+frankzohim@users.noreply.github.com>
2022-08-09 10:25:52 +0200

    commit from monaco, footer and header updated

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -64,25 +64,9 @@
 							<nav class="header__nav header-nav">
 								<ul class="header-nav__list">
 
-
-									<li class="dropdown dropdown_hover-drop dropdown_theme_arrow header-nav__item header-nav__dropdown" data-test="header_nav__company">
-										Entreprise
-
-										<svg class="icon dropdown__arrow"><use href="#arrow-down-alt"></use></svg>
-
-										<div class="dropdown__drop">
-
-											<ul class="dropdown__list" data-test="dropdown-list__company">
-
-												<li class="dropdown__item"><a class="link_inherit" data-test="header_nav__about" href="#"><span class="header-nav__name">Qui Sommes Nous?</span></a></li>
-												<li class="dropdown__item"><a class="link_inherit" data-test="header_nav__community" href="#"><span class="header-nav__name">Intervenants</span></a></li>
-												<li class="dropdown__item"><a class="link_inherit" data-test="header_nav__partnership" href="#"><span class="header-nav__name">Franchises</span></a></li>
-
-											</ul><div class="dropdown__drop-arrow"></div>
-
-										</div>
+									<li class="header-nav__item"><a class="link_inherit" data-test="header_nav__blog" href="{{ route('homepage') }}">Accueil</a>
 									</li>
-
+									
 
 									<li class="dropdown dropdown_hover-drop dropdown_theme_arrow header-nav__item header-nav__dropdown" data-test="header_nav__company">
 										Nos Services
@@ -144,15 +128,16 @@
 
 
 									<li class="dropdown dropdown_hover-drop dropdown_theme_arrow header-nav__item header-nav__dropdown" data-test="header_nav__help">
-										Notre Fonctionnement
+										Fonctionnement
 
 									</li>
 
 
-									<li class="header-nav__item"><a class="link_inherit" data-test="header_nav__pricing" href="#">Formations</a>
+									<li class="header-nav__item"><a class="link_inherit" data-test="header_nav__pricing" href="{{ route('about') }}">Qui sommes-nous ?</a>
 									</li>
 
-
+									<li class="header-nav__item"><a class="link_inherit" data-test="header_nav__pricing" href="#">Contact</a>
+									</li>
 
 
 
