59bc748d02046b5074d8f2d78027fafeffda5d67
Frank  Zohim <69459460+frankzohim@users.noreply.github.com>
2022-08-27 15:37:02 +0200

    commit from monaco, services pages updated, footer updating

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,31 @@
+		
+		<style>
+
+			footer .footer-logo img{max-height:36px}
+			footer ul{padding-left:0;list-style:none}
+			footer ul li{margin-bottom:7px}
+			footer ul li:last-child{margin-bottom:0}
+			footer .footer-top ul li.media{margin-bottom:25px}
+			footer .footer-top ul li.media:last-child{margin-bottom:0}
+			footer .instagram-follow-api ul li{margin-bottom:0}
+			footer .footer-horizontal-link li{margin-right:35px;list-style:none;margin-bottom:0}
+			footer .footer-horizontal-link li:last-child{margin-right:0}
+			.footer-dark,.footer-dark a{color:#8890a4}
+			.footer-dark a:hover{color:#fff}
+			.footer-dark .footer-social a{color:#fff}
+			.footer-dark .footer-social a:hover{color:#8890a4}
+			.footer-sticky{position:fixed;width:100%;bottom:0;left:0;right:0;z-index:0}
+			.scroll-top-arrow,.scroll-top-arrow:focus{background:#fff;font-size:17px;line-height:34px;box-shadow:0 0 25px rgba(23,23,23,.25);display:none;height:34px;width:34px;padding:0;position:fixed;right:45px;text-align:center;text-decoration:none;bottom:45px;z-index:1029;border-radius:100%}
+			.scroll-top-arrow:hover{background:#fff;color:#828282;box-shadow:0 0 30px rgba(23,23,23,.4);-webkit-transform:translateY(-2px);-ms-transform:translateY(-2px);transform:translateY(-2px)}
+			footer .instafeed-grid.gutter-small{margin-left:-5px;margin-right:-5px}
+			footer .instafeed-grid.gutter-very-small{margin-left:-3px;margin-right:-3px}
+			footer .instafeed-grid.gutter-medium{margin-left:-7px;margin-right:-7px}
+			footer .instafeed-grid.gutter-large{margin-left:-10px;margin-right:-10px}
+			footer .instafeed-grid.gutter-extra-large{margin-left:-15px;margin-right:-15px}
+			footer .instafeed-grid.gutter-double-extra-large{margin-left:-20px;margin-right:-20px}
+			.bg-slate-blue{background-color:#262b35}
+			.padding-five-tb{padding-top:5%;padding-bottom:5%}
+		</style>
 		<footer class="footer">
 				<div class="container">
 					<div class="margin-top-lg footer__actions">
@@ -27,11 +55,11 @@
 						<div class="footer__nav-wrap">
 
 							<ul class="footer-nav">
-								<li class="text caps">Qui Sommes Nous?</li>
-								<li class="text text_md color_grey"><a class="link" data-inner-link="true" href="{{ route('about') }}">A propos </a></li>
-								<li class="text text_md color_grey"><a class="link" data-inner-link="true" href="{{ route('commitment') }}">Nos engagements</a></li>
-								<li class="text text_md color_grey"><a class="link" data-inner-link="true" href="{{ route('quality') }}">Charte de qualité</a></li>
-								<li class="text text_md color_grey"><a class="link" data-inner-link="true" href="#">Les avantages</a></li>
+								<li class="text caps">Services</li>
+								<li class="text text_md color_grey"><a class="link" data-inner-link="true" href="#">Aide à la vie social </a></li>
+								<li class="text text_md color_grey"><a class="link" data-inner-link="true" href="#">Aide au repas</a></li>
+								<li class="text text_md color_grey"><a class="link" data-inner-link="true" href="#">Aide à l’autonomie</a></li>
+								<li class="text text_md color_grey"><a class="link" data-inner-link="true" href="#">Aide à l’accompagnement</a></li>
 							</ul>
 
 							<ul class="footer-nav">
@@ -68,3 +96,77 @@
 				</div>
 
 		</footer>
+
+		  <!-- start footer  -->
+        <footer class="footer-dark bg-slate-blue">
+            <div class="footer-top padding-five-tb lg-padding-eight-tb md-padding-50px-tb">
+                <div class="container">
+                    <div class="row">
+                        <!-- start footer column -->
+                        <div class="col-12 col-lg-3 col-sm-6 md-margin-40px-bottom xs-margin-25px-bottom">
+                            <span class="alt-font font-weight-500 d-block text-white margin-20px-bottom xs-margin-10px-bottom">Company</span>
+                            <ul>
+                                <li><a href="about-us.html">About company</a></li>
+                                <li><a href="our-services.html">Company services</a></li>
+                                <li><a href="our-team.html">Job opportunities</a></li>
+                                <li><a href="contact-us-classic.html">Contact us</a></li>
+                            </ul>
+                        </div>
+                        <!-- end footer column -->
+                        <!-- start footer column -->
+                        <div class="col-12 col-lg-3 col-sm-6 md-margin-40px-bottom xs-margin-25px-bottom">
+                            <span class="alt-font font-weight-500 d-block text-white margin-20px-bottom xs-margin-10px-bottom">Customer</span>
+                            <ul>                           
+                                <li><a href="faq.html">Client support</a></li>
+                                <li><a href="pricing-packages.html">Pricing packages</a></li>
+                                <li><a href="our-story.html">Company history</a></li>
+                                <li><a href="our-process.html">Our process</a></li>
+                            </ul>
+                        </div>
+                        <!-- end footer column -->
+                        <!-- start footer column -->
+                        <div class="col-12 col-lg-3 col-sm-6 xs-margin-25px-bottom">
+                            <span class="alt-font font-weight-500 d-block text-white margin-20px-bottom xs-margin-10px-bottom">Get in touch</span>
+                            <p class="w-85 margin-15px-bottom">27 Eden walk eden centre, Orchard view, Paris, France</p>
+                            <div><i class="feather icon-feather-phone-call icon-very-small margin-10px-right text-white"></i>+1 234 567 8910</div>
+                            <div><i class="feather icon-feather-mail icon-very-small margin-10px-right text-white"></i><a href="#">info@yourdomain.com</a></div>
+                        </div>
+                        <!-- end footer column -->
+                        <!-- start footer column -->
+                        <div class="col-12 col-lg-3 col-sm-6">
+                            <span class="alt-font font-weight-500 d-block text-white margin-20px-bottom">Follow us on Instagram</span>
+                            <div class="w-100 d-inline-block margin-10px-top">
+                                <ul class="instafeed-grid instafeed-wrapper grid grid-3col xl-grid-3col lg-grid-3col md-grid-3col sm-grid-3col xs-grid-3col gutter-small" data-total="3">
+                                    <li class="grid-item"><figure><a href="#" data-href="" target="_blank"><img src="#" data-src="" class="insta-image" alt="" /><span class="insta-counts"><i class="fab fa-instagram"></i></span></a></figure></li>
+                                </ul>
+                                <a class="alt-font text-extra-small text-uppercase font-weight-500 margin-20px-top d-inline-block" href="https://www.instagram.com/" target="_blank"><i class="fab fa-instagram icon-extra-small align-middle margin-10px-right text-gradient-light-purple-light-orange"></i><span class="d-inline-block align-middle">Follow instagram</span></a>
+                            </div>
+                        </div>
+                        <!-- end footer column -->
+                    </div>
+                </div>
+            </div>
+            <div class="footer-bottom padding-40px-tb border-top border-color-white-transparent">
+                <div class="container"> 
+                    <div class="row align-items-center">
+                        <div class="col-12 col-md-3 text-center text-md-start sm-margin-20px-bottom">
+                            <a href="index.html" class="footer-logo"><img src="images/logo-white.png" data-at2x="images/logo-white@2x.png" alt=""></a>
+                        </div>
+                        <div class="col-12 col-md-6 text-center last-paragraph-no-margin sm-margin-20px-bottom">
+                            <p>&copy; 2021 Litho is Proudly Powered by <a href="https://www.themezaa.com/" target="_blank">ThemeZaa</a></p>
+                        </div>
+                        <div class="col-12 col-md-3 text-center text-md-end">
+                            <div class="social-icon-style-12">
+                                <ul class="extra-small-icon light">
+                                    <li><a class="facebook" href="https://www.facebook.com/" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li><a class="dribbble" href="http://www.dribbble.com" target="_blank"><i class="fab fa-dribbble"></i></a></li>
+                                    <li><a class="twitter" href="http://www.twitter.com" target="_blank"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a class="instagram" href="http://www.instagram.com" target="_blank"><i class="fab fa-instagram"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- end footer -->
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -503,6 +503,7 @@ a,abbr,acronym,address,applet,article,aside,audio,b,big,blockquote,body,canvas,c
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
   <script src="{{ asset('assets/js/collapse.js') }}"></script>
+  
 
    <!-- Loading js file write by me that enable bootstrap tap -->
 
