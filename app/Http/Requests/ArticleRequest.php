056701b23c346fdf300fb2e7f6a081d5dced67ca
level237 <Bramslevel129@gmail.com>
2022-08-31 10:22:45 -0400

    request init:resource Article

---
+++ app/Http/Requests/ArticleRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ArticleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'title'=>['required'],
+            'contenue'=>['required'],
+            'image_path'=>['required','image'],
+            'meta_description'=>['required'],
+            'keywords'=>['required'],
+            'status'=>['required']
+        ];
+    }
+}
