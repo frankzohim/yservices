86a4aadccf2a5b6830ac85b33e074cf8b1fb0670
level237 <Bramslevel129@gmail.com>
2022-09-29 15:57:35 -0400

    updte migration

--- database/migrations/2022_08_06_142920_create_needs_table.php
+++ database/migrations/2022_08_06_142920_create_needs_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\User;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -13,6 +14,7 @@ return new class extends Migration
      */
     public function up()
     {
+        Schema::disableForeignKeyConstraints();
         Schema::create('needs', function (Blueprint $table) {
             $table->id();
             $table->string('services');
@@ -27,6 +29,11 @@ return new class extends Migration
             $table->string('email');
             $table->string('phone');
             $table->string('address');
+
+            $table->foreignIdFor(User::class)
+            ->constrained()
+            ->onUpdate('restrict')
+            ->onDelete('restrict');
             $table->timestamps();
         });
     }
