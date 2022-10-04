2c250754fbe79dee5b99237cb6519e30e33e00d4
level237 <Bramslevel129@gmail.com>
2022-10-04 09:41:27 -0400

    update

--- resources/views/layouts/backoffice/app.blade.php
+++ resources/views/layouts/backoffice/app.blade.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html lang="en">
 <head>
+    @livewireStyles
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta http-equiv="Content-Language" content="en">
